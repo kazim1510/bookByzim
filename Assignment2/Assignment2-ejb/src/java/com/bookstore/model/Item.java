da492654ff391d62bbcd32c5ce3fea1e1accf81f
HP <HP@qazim-PC>
2015-10-17 19:17:53 +1100

    Item Model And Subscription Model Created

---
+++ Assignment2/Assignment2-ejb/src/java/com/bookstore/model/Item.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookstore.model;
+
+import javax.persistence.*;
+
+/**
+ *
+ * @author HP
+ */
+public class Item {
+
+    private int itemId;
+    private String itemName;
+    private String itemDescp;
+    
+    @ManyToOne
+    private Subscription subscription;
+    
+    @Id
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDescp() {
+        return itemDescp;
+    }
+
+    public void setItemDescp(String itemDescp) {
+        this.itemDescp = itemDescp;
+    }
+
+    public Subscription getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(Subscription subscription) {
+        this.subscription = subscription;
+    }
+}
---
+++ Assignment2/Assignment2-ejb/src/java/com/bookstore/model/Subscription.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookstore.model;
+
+import java.util.Date;
+import javax.persistence.*;
+
+/**
+ *
+ * @author HP
+ */
+@Entity
+public class Subscription {
+   
+    private int subsId;
+    private String subsType;
+    
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+    
+    @Temporal(TemporalType.DATE)
+    private Date endDate;
+    
+    private int pricePerMonth;
+    
+    @Id
+    public int getSubsId() {
+        return subsId;
+    }
+
+    public void setSubsId(int subsId) {
+        this.subsId = subsId;
+    }
+
+    public String getSubsType() {
+        return subsType;
+    }
+
+    public void setSubsType(String subsType) {
+        this.subsType = subsType;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getPricePerMonth() {
+        return pricePerMonth;
+    }
+
+    public void setPricePerMonth(int pricePerMonth) {
+        this.pricePerMonth = pricePerMonth;
+    }
+    
+    
+    
+}
