d7a2913ab7cfbb7c6d98fba92278dd2da4759aaa
HP <HP@qazim-PC>
2015-10-18 22:13:28 +1100

    Account Controller modified

--- Assignment2/Assignment2-war/src/java/com/bookstore/controller/AccountController.java
+++ Assignment2/Assignment2-war/src/java/com/bookstore/controller/AccountController.java
@@ -5,10 +5,44 @@
  */
 package com.bookstore.controller;
 
+import com.bookstore.DB.AccountDAO;
+import com.bookstore.model.Account;
+import javax.faces.context.FacesContext;
+import javax.inject.*;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  *
  * @author HP
  */
 public class AccountController {
+    
+    @Inject
+    private AccountDAO accountdao;
+    
+    Account account = new Account();
+    
+    public Account getAccount(){
+        return account;
+    }
+    
+    public void login(SessionController sessionContrller){
+        
+        FacesContext context = FacesContext.getCurrentInstance();
+        
+        try{
+            getRequest().login(account.getUsername(), account.getPassword());
+            sessionContrller.setAccount(accountdao.get(account.getUsername()));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error");
+        }
+    }
+    
+    private static HttpServletRequest getRequest() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        return (HttpServletRequest)context.getExternalContext().getRequest();
+    }
     
 }
---
+++ Assignment2/Assignment2-war/src/java/com/bookstore/controller/SessionController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookstore.controller;
+
+import com.bookstore.model.Account;
+
+/**
+ *
+ * @author HP
+ */
+public class SessionController {
+
+    private Account account;
+
+    
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+    
+    
+}
